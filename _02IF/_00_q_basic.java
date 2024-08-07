4f6c8ae80f35c18275c86d9dff6dccc25b254733
kimsion <tkdlqhrm222@gmail.com>
2024-08-07 15:34:59 +0900

    ififquestion

--- _02IF/_00_q_basic.java
+++ _02IF/_00_q_basic.java
@@ -4,6 +4,13 @@ public class _00_q_basic {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Basic
+		//화살의 위치가 0에서 12사이이면 +10점;
+		//13~20점 사이이면 +9점
+		//21~30 사이이면 +8점
+		//31이상이면 +7점을 부여한다.
+		
+		
 		int point = 20;
 		int x = 1; // x는 화살의 위치
 		
@@ -30,8 +37,18 @@ public class _00_q_basic {
 		
 		System.out.println(point);
 		
-		point = 20;
+		
+		
+		//up1
+		//0부터 12사이면 +10점을 획득하고
+		//0-1 사이이면 보너스 점수를 획득한다. 
+		//보너스 점수는 현재 point의 10% 이다.
+		
+		
 		//up1 solution
+		point = 20;
+		x = -1;
+		
 		if(x >= 0 && x <= 12) {
 			if( x>0 && x<=1) {
 				point += point*0.1;

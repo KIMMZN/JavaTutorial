b1f9d4e0e3a177d6e89c5ec245967ad62e6536b7
sionkim <tkdlqhrm222@gmail.com>
2024-08-09 07:30:37 +0900

    morning

--- _03FOR/_00_FOR_copy.java
+++ _03FOR/_00_FOR_copy.java
@@ -1,6 +1,6 @@
 package _03FOR;
 
-public class _00_FOR {
+public class _00_FOR_copy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -14,12 +14,14 @@ public class _00_FOR {
 		}
 		
 		// 40 50 55 65 30
-		for(int i=1; i<=5; i++) {
-			System.out.println("dd " + a[i%5]);
+		
+		for(int i =1; i <= 5; i++) {
+			System.out.println("aa "+ a[i%5]); //for문의 구조와 %에 대해서 이해해야한다.
 		}
 		
-		System.out.println("111 " + 5%5);
-		
+		for(int i = 0; i<=4; i++) {
+			System.out.println("bb" + a[(i+1)%5]);
+		}
 		
 		/*for(int = 0; i<=0; i++) {
 			sysout(a[(i+1)%5])
@@ -32,6 +34,11 @@ public class _00_FOR {
 				System.out.println(a[i]);
 			}
 		}
+		
+		for(int i=0; i<5; i++)
+			if (a[i] >= 50) {
+				System.out.println("fi" + a[i]);
+			}
 		
 		
 		//50점 이상 카운트
@@ -58,6 +65,12 @@ public class _00_FOR {
 		}
 		 System.out.println(cnt1);
 		 
+		 int avrn = 0;
+		 for(int i=0;i < 5; i++ ) {
+			 if(a[i] >= 50) {
+				 avrn += a[i];
+			 }
+		 } System.out.println("50점 이상:" + avrn);
 		 //2. 평균을 구하시오 단. 50점이상;
 		 
 		 int count = 0;
@@ -70,8 +83,19 @@ public class _00_FOR {
 			}
 			 System.out.println(cnt2/count);
 			 
+			 //
+			 int divnum = 0;
+			 int avrn1 = 0;
+			 for(int i=0;i < 5; i++ ) {
+				 if(a[i] >= 50) {
+					 avrn1 += a[i];
+					 divnum++;
+				 }
+			 } System.out.println("50점 이상 평균:" + avrn1/divnum);
+			 
 		//3. 평균을 구하시오; 단 최저점수는 포함하지 마시오;
 			 int[] a1 = {30, 40, 50, 55, 65};
+			 
 			 
 			 int sum = 0;
 			 int cnta = 0;
@@ -97,7 +121,25 @@ public class _00_FOR {
 			  
 			 System.out.println((double)sum/cnta);
 			 
-			
+			 //  평균을 구하시오; 단 최저점수는 포함하지 마시오;
+			 // 합쳐보기;;
+			 int[] a2 = {30, 40, 50, 55, 65};
+			 
+			 int minval2 = 10000;
+			 int avgcount = 0;
+			 int avgsum = 0;
+			 
+			 for(int i = 0; i<5; i++) {
+				 
+				 if (a2[i] < minval2) {
+					 minval2 = a2[i];
+					 
+				 }
+				 avgcount++;
+				 avgsum += a[i];
+			 } System.out.println((double)(avgsum-minval2)/(avgcount-1));
+			 
+			 
 		
 		
 		

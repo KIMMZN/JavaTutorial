b4aa0f38649a50c9fadf8fab0d623f69156cbddf
sionkim <tkdlqhrm222@gmail.com>
2024-08-12 22:26:51 +0900

    0812forwork

--- _03FOR_0812/For0812_work_04.java
+++ _03FOR_0812/For0812_work_04.java
@@ -2,41 +2,31 @@ package _03FOR_0812;
 
 public class For0812_work_04 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		//
-		//4 난건물주
-		//int[] arr = {1,0,0,0,1,1,1,0,0,0,0,1,1,1,1,0,0,0,1}
-		//int size=2;
-		//arr 배열에서 0이 의미하는 것은 공터이다. 
-		//size는 건물의 크기 이다. 건물은 하나만 짓는다. 공터는 연속적으로 있어야 한다. 
-		//size 설정값에 따라 공터에 건물을 지을 수 있는 위치는 모두 몇개인가?
-		//size가 2일경우에 7개이다. 
-		
-		// for문 arr.length
-		// 0 , 0 겹쳐야 건물 1개;
-		//
-		
-		int[] arr = {1,0,0,0,1,1,1,0,0,0,0,1,1,1,1,0,0,0,1};
-		
-		int size=2;
-		
-		int countn = 0;
-		
-		int lnumber = arr[0];
-		for(int i = 0; i < arr.length; i++ ) {
-			
-			if(arr[i] == lnumber && arr[i] == 0) {
-				
-				countn++;
-			} else {
-				lnumber = arr[i];
-			}
-			
-		} 
-		System.out.println("답은: " + countn);
-
-	}
-
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1};
+        int size = 3;  // 건물의 크기
+        
+        int count = 0;  // 가능한 위치의 개수
+        int currentLength = 0;  // 현재 연속된 0의 길이
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                currentLength++;  // 연속된 0의 길이를 증가시킴
+            } 
+            else {
+                if (currentLength >= size) {
+                    // 연속된 0의 길이가 size 이상일 경우, 위치를 계산
+                    count += (currentLength - size + 1);
+                }
+                currentLength = 0;  // 연속된 0의 길이를 초기화
+            }
+        }
+        
+        // 배열의 끝에서 연속된 0의 길이를 다시 한 번 확인
+        if (currentLength >= size) {
+            count += (currentLength - size + 1);
+        }
+        
+        System.out.println("건물을 지을 수 있는 위치의 개수: " + count);
+    }
 }
--- _03FOR_0812/For0812_work_07.java
+++ _03FOR_0812/For0812_work_07.java
@@ -5,27 +5,26 @@ public class For0812_work_07 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-				
-				/*7
-				철수는 배열의 0번 인덱스부터 순회한다.
-				0번 인덱스에서는 0번인덱스와 그 다음 인덱스를 비교하여 큰 수를 뒤로 배치한다.
-				이러한 방식으로 배열의 뒤까지 반복하면 맨 뒤에 가장 큰 수가 배치 될 것이다.
-				위와 같은 방식으로 가장큰 수를 구하시오. */
-			         //0  1  2 3 4  5
-		    int[] a = {34,2,35,8,31,45};
-			int lval = 0;
-			
-			for(int i =0; i < a.length; i++) {
-				
-				
-				
-				
-				
-		}	
-			
-			
-			System.out.println(a[5]);
-				   
-	}
 
+	   
+	        int[] a = {34, 2, 35, 8, 31, 45};
+
+	        // 배열을 순회하면서 큰 값을 뒤로 이동시키기
+	        for (int i = 0; i < a.length - 1; i++) {
+	            if (a[i] > a[i + 1]) {
+	            	// 34 > 2
+	                // 두 값을 직접 교환
+	                a[i] = a[i] + a[i + 1];
+	                //36 = 34 + 2
+	                a[i + 1] = a[i] - a[i + 1];
+	                //32 = 34 - 2
+	                a[i] = a[i] - a[i + 1];
+	                //34 = 36 - 32
+	            }
+	        }
+
+	        // 가장 큰 값 출력
+	        System.out.println("가장 큰 값: " + a[a.length - 1]);
+	        System.out.println(a.length);
+	    }
 	}

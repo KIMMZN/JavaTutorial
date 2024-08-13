ccaf82166d4e152d4a9f38cea0ed3e3f339ba711
kimsion <tkdlqhrm222@gmail.com>
2024-08-13 17:31:40 +0900

    for081300fr5

--- _03FOR_0812/For0812_work_07.java
+++ _03FOR_0812/For0812_work_07.java
@@ -4,38 +4,28 @@ public class For0812_work_07 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
-		 /* 7
-			철수는 배열의 0번 인덱스부터 순회한다.
-			0번 인덱스에서는 0번인덱스와 그 다음 인덱스를 비교하여 큰 수를 뒤로 배치한다.
-			이러한 방식으로 배열의 뒤까지 반복하면 맨 뒤에 가장 큰 수가 배치 될 것이다.
-			위와 같은 방식으로 가장큰 수를 구하시오. */
-		
-		    //if a[i] >= a[i+1]
-		   	//a[a+1] = a[i]
-		     
-		     //else { a[a+1] ;
-		
-			int[] a = {34,2,35,8,31,45};
-			
-			int temp = 0;
-			for(int i =0; i < a.length-1; i++) {
-				
-				if (a[i] >= a[i+1]) {
-					temp = a[i];
-					//System.out.println(temp);
-					
-				}
-				else {
-					temp = a[i+1]; 
-				}
-				
-			}
-			a[5] = temp;
-			
-			
-			 System.out.println(" a5 " + a[5]);
-			 
-			 
+	   
+	        int[] a = {34, 2, 35, 8, 31, 45};
+
+	        // 배열을 순회하면서 큰 값을 뒤로 이동시키기
+	        for (int i = 0; i < a.length - 1; i++) {
+	            if (a[i] > a[i + 1]) {
+	            
+	              
+	                a[i] = a[i] + a[i + 1];
+	               
+	                a[i + 1] = a[i] - a[i + 1];
+	                
+	                a[i] = a[i] - a[i + 1];
+	                
+	            }
+	        }
+
+	        // 가장 큰 값 출력
+	        System.out.println("가장 큰 값: " + a[a.length - 1]);
+	        System.out.println(a.length);
+	        System.out.println(a[5]);
 	    }
 	}

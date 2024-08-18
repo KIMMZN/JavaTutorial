f7ab1baff7e2e84f441a1d467e0e108a4d9b3331
sionkim <tkdlqhrm222@gmail.com>
2024-08-19 02:14:31 +0900

    0819

---
+++ _test01/_Test_01.java
@@ -0,0 +1,18 @@
+package _test01;
+
+public class _Test_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i = 1; i < 10; i++) {
+			System.out.println(i + "단");
+			for (int j = 1; j<10; j++) {
+				System.out.println(i + "x" + j + "=" + (i*j));
+			}
+			
+		}
+
+	}
+
+}
